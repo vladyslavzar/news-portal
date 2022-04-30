a6014171adee1a0e3842cb2dd17cd1947d14a570
vladyslavzar <zarubinvlad12@gmail.com>
2022-04-30 22:31:58 +0300

    cors fix

--- src/utils/news.util.js
+++ src/utils/news.util.js
@@ -2,7 +2,13 @@ const _apiKey = '86093b2ae8ab4a84b5957f0c566ab1f3';
 const proxyUrl = "https://thingproxy.freeboard.io/fetch/"
 
 export const getHotTopic = async () => {
-    const res = await fetch(`/v2/top-headlines?language=en&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/top-headlines?language=en&apiKey=${_apiKey}`,{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+  
+      });
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
@@ -15,7 +21,13 @@ export const getHotTopic = async () => {
 }
 
 export const getTopicsInCategory = async (category, page = 1) => {
-    const res = await fetch(`/v2/top-headlines?language=en&category=${category}&page=${page}&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/top-headlines?language=en&category=${category}&page=${page}&apiKey=${_apiKey}`, {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+  
+      });
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
@@ -28,7 +40,13 @@ export const getTopicsInCategory = async (category, page = 1) => {
 }
 
 export const getTopicsBySearch = async (request, page = 1) => {
-    const res = await fetch(`/v2/everything?q=${request}&page=${page}&sortBy=publishedAt&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/everything?q=${request}&page=${page}&sortBy=publishedAt&apiKey=${_apiKey}`, {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+  
+      });
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
@@ -41,7 +59,13 @@ export const getTopicsBySearch = async (request, page = 1) => {
 }
 
 export const getTopicByTitle = async (request, page = 1) => {
-    const res = await fetch(`/v2/everything?q=${request}&page=${page}&searchIn=title&sortBy=publishedAt&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/everything?q=${request}&page=${page}&searchIn=title&sortBy=publishedAt&apiKey=${_apiKey}`, {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+  
+      });
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
