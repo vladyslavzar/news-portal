84d596e6cbf194264e606763ced8432f1ce3cf93
vladyslavzar <zarubinvlad12@gmail.com>
2022-04-30 22:24:25 +0300

    Cors fix

--- src/utils/news.util.js
+++ src/utils/news.util.js
@@ -2,7 +2,7 @@ const _apiKey = '86093b2ae8ab4a84b5957f0c566ab1f3';
 const proxyUrl = "https://thingproxy.freeboard.io/fetch/"
 
 export const getHotTopic = async () => {
-    const res = await fetch(`${proxyUrl}https://newsapi.org/v2/top-headlines?language=en&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/top-headlines?language=en&apiKey=${_apiKey}`);
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
@@ -15,7 +15,7 @@ export const getHotTopic = async () => {
 }
 
 export const getTopicsInCategory = async (category, page = 1) => {
-    const res = await fetch(`${proxyUrl}https://newsapi.org/v2/top-headlines?language=en&category=${category}&page=${page}&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/top-headlines?language=en&category=${category}&page=${page}&apiKey=${_apiKey}`);
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
@@ -28,7 +28,7 @@ export const getTopicsInCategory = async (category, page = 1) => {
 }
 
 export const getTopicsBySearch = async (request, page = 1) => {
-    const res = await fetch(`${proxyUrl}https://newsapi.org/v2/everything?q=${request}&page=${page}&sortBy=publishedAt&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/everything?q=${request}&page=${page}&sortBy=publishedAt&apiKey=${_apiKey}`);
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
@@ -41,7 +41,7 @@ export const getTopicsBySearch = async (request, page = 1) => {
 }
 
 export const getTopicByTitle = async (request, page = 1) => {
-    const res = await fetch(`${proxyUrl}https://newsapi.org/v2/everything?q=${request}&page=${page}&searchIn=title&sortBy=publishedAt&apiKey=${_apiKey}`);
+    const res = await fetch(`/v2/everything?q=${request}&page=${page}&searchIn=title&sortBy=publishedAt&apiKey=${_apiKey}`);
 
     if (!res.ok) {
         console.log(`Error, error status: ${res.status}`);
