171f8cccc8b27e9b47f1b4c7ad29c67d33e8d7ad
vladyslavzar <zarubinvlad12@gmail.com>
2022-05-03 11:07:28 +0300

    view fix

--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -57,7 +57,7 @@ const Article = ({setIsLoading, isLoading}) => {
                                                     <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" className="style-scope yt-icon" ><g className="style-scope yt-icon"><path d="M17,4h-1H6.57C5.5,4,4.59,4.67,4.38,5.61l-1.34,6C2.77,12.85,3.82,14,5.23,14h4.23l-1.52,4.94C7.62,19.97,8.46,21,9.62,21 c0.58,0,1.14-0.24,1.52-0.65L17,14h4V4H17z M10.4,19.67C10.21,19.88,9.92,20,9.62,20c-0.26,0-0.5-0.11-0.63-0.3 c-0.07-0.1-0.15-0.26-0.09-0.47l1.52-4.94l0.4-1.29H9.46H5.23c-0.41,0-0.8-0.17-1.03-0.46c-0.12-0.15-0.25-0.4-0.18-0.72l1.34-6 C5.46,5.35,5.97,5,6.57,5H16v8.61L10.4,19.67z M20,13h-3V5h3V13z" className="style-scope yt-icon"></path></g></svg>
                                                     <span>{item.dislikes ? item.dislikes : ''}</span>
                                                 </div> */}
-                                                <h5 style={{display: `${user && (item.email === user.email) ? 'inline' : 'none'}`}} onClick={() => {
+                                                <h5 style={{display: `${user && (item.email === user.email) ? 'inline' : 'none'}`}} className="deletion" onClick={() => {
                                                     if (item.email !== user.email) return;
 
                                                     deleteComment(newArr[0].id, item.id);
@@ -90,7 +90,7 @@ const Article = ({setIsLoading, isLoading}) => {
                         <input type="text" placeholder="Write a comment" value={commInpt} onChange={(e) => {setCommInpt(e.target.value)}}/>
                         <button onClick={btnHandler}>Post</button>
                     </div>
-                    view
+                    {view}
                 </div>
             </div>
         </section>
